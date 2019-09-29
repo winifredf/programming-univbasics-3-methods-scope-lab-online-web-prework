55a27d89ab12b0e729e2e8b6229012a62d25210b
Winifred Frierson <winifred.dykes@hotmail.com>
2019-09-29 18:34:23 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -2,7 +2,10 @@ def mario
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase = "It's-a me, Mario!"
   
+  end
+end
 
 def toadstool
+  status = 
   puts status
 end
